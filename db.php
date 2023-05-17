11a9cb321d32ae9c6967cd24be376729d998b5d2
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-17 17:05:29 +0200

    add more pcs in created db file

---
+++ db.php
@@ -0,0 +1,40 @@
+<?php
+$personalPcs = [
+   $pc_1 = new Desktop(
+      "MSI",
+      "M-COMP-12445",
+      "Intel Core i7",
+      "2 x 8",
+      "1T",
+      "ASUS Tuf Gaming ABC-23-Z",
+      "NVIDIA GeForce RTX 4020"
+   ),
+
+   $pc_2 = new Laptop(
+      "Asus",
+      "ZenBook",
+      "Intel Core i5",
+      "8",
+      "512 SSD",
+      "15",
+      "8000"
+   ),
+   $pc_3 = new Desktop(
+      "Dell",
+      "HH-JK12",
+      "Intel Core i3",
+      "4",
+      "500 Gb",
+      "ASUS Prime B450M-A",
+      "AMD Radeon RX 600 XT"
+   ),
+   $pc_4 = new Laptop(
+      "Lenovo",
+      "Legion",
+      "Intel Core i9-12900HX",
+      "2 x 16",
+      "2T SSD",
+      "16",
+      "10000"
+   ),
+];
