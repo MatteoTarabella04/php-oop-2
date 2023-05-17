bdf9db09a77fe44477cf5dce217a2b877287b1b9
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-17 16:25:27 +0200

    create desktop + laptop classes

--- index.php
+++ index.php
@@ -40,6 +40,24 @@ class Computer
       return $this->cpu;
    }
 }
+class Desktop extends Computer
+{
+   public function __construct(protected string $brand, protected string $model, protected string $cpu, protected string $ram, protected string $storage, protected string $gpu, protected string $mbo)
+   {
+      parent::__construct($brand, $model, $cpu, $ram, $storage);
+      $this->gpu = $gpu;
+      $this->mbo = $mbo;
+   }
+}
+class Laptop extends Computer
+{
+   public function __construct(protected string $brand, protected string $model, protected string $cpu, protected string $ram, protected string $storage, protected string $screen_size, protected string $battery)
+   {
+      parent::__construct($brand, $model, $cpu, $ram, $storage);
+      $this->screen_size = $screen_size;
+      $this->battery = $battery;
+   }
+}
 
 $personalPcs = [
    $pc = new Computer('Asus', 'Zenbook', 'Rayzen 7', '2 x 8', '1T'),
