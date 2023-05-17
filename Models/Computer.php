34478105e55e8c18761903d64422e4fed0bfcabc
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-05-17 17:05:48 +0200

    create model computer

---
+++ Models/Computer.php
@@ -0,0 +1,27 @@
+<?php
+class Computer
+{
+   public function __construct(protected string $brand, protected string $model, protected string $cpu, protected string $ram, protected string $storage)
+   {
+      $this->brand = $brand;
+      $this->model = $model;
+      $this->cpu = $cpu;
+      $this->ram = $ram . 'Gb';
+      $this->storage = $storage;
+   }
+
+   public function getBrand()
+   {
+      return $this->brand;
+   }
+
+   public function getModel()
+   {
+      return $this->model;
+   }
+
+   public function getCpu()
+   {
+      return $this->cpu;
+   }
+}
